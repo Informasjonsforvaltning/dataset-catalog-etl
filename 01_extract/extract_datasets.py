7a628e00cc4f7b467b581dc2f0aed3e35616df88
Hans Olav Sund <exthos@brreg.no>
2020-04-30 14:25:20 +0200

    Chore/hos/back to basics (#2)
    * chore: started extract for dataset_fields, set up field structure, converted tablefields to dict, set up field structure, working extract for all dataset fields

--- 01_extract/extract_datasets.py
+++ 01_extract/extract_datasets.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
  
-sqlEngine       = create_engine('mysql+pymysql://usr:pwd@127.0.0.1', pool_recycle=3600)
+sqlEngine       = create_engine('mysql+pymysql://user:password@127.0.0.1', pool_recycle=3600)
 dbConnection    = sqlEngine.connect()
 frame           = pd.read_sql("""
 SELECT nid
@@ -11,7 +11,7 @@ FROM
   drupal_datanorge.node
 WHERE
   type = 'data'
-""", dbConnection);
+""", dbConnection)
  
 pd.set_option('display.expand_frame_repr', False)
 frame.to_json(r'../tmp/datasets.json')
---
+++ 01_extract/fields.py
@@ -0,0 +1,42 @@
+fields = ["field_data_body","field_data_comment_body","field_data_field_access_rights","field_data_field_access_url","field_data_field_accrual_periodicity","field_data_field_antall_datasett","field_data_field_app_contact_mail","field_data_field_app_contact_name","field_data_field_app_description","field_data_field_app_resource","field_data_field_approved","field_data_field_category","field_data_field_data","field_data_field_data_contact_mail","field_data_field_data_contact_name","field_data_field_data_description","field_data_field_data_resource","field_data_field_data_resources_data","field_data_field_data_version","field_data_field_dcat_key","field_data_field_delete","field_data_field_description","field_data_field_distribution","field_data_field_distribution_documentation","field_data_field_distribution_title","field_data_field_distribution_type","field_data_field_docs","field_data_field_documentation","field_data_field_download_url","field_data_field_file_image_alt_text","field_data_field_file_image_title_text","field_data_field_format","field_data_field_formatlist","field_data_field_geo","field_data_field_hidden_comment","field_data_field_image","field_data_field_images","field_data_field_ingress","field_data_field_key","field_data_field_landing_page","field_data_field_language","field_data_field_licence_software","field_data_field_license","field_data_field_lisens","field_data_field_los","field_data_field_organization","field_data_field_organization_homepage","field_data_field_organization_mail","field_data_field_organization_number","field_data_field_sektorkode","field_data_field_sektornavn","field_data_field_shortname","field_data_field_slide_image","field_data_field_slide_number","field_data_field_slide_text","field_data_field_slide_url","field_data_field_slideshow_image","field_data_field_slideshow_number","field_data_field_slideshow_title","field_data_field_temporal","field_data_field_temporal_end","field_data_field_temporal_start","field_data_field_tfaq","field_data_field_theme_uri","field_data_field_type","field_data_field_uri","field_data_field_url","field_data_field_user_org_ref","field_data_field_user_organization","field_data_field_web_service","field_data_title_field"]
+
+# dataset 77
+dataset_fields = ["field_data_field_access_rights",
+                  "field_data_field_antall_datasett",
+                  "field_data_field_approved",
+                  "field_data_field_category",
+                  "field_data_field_data_contact_mail",
+                  "field_data_field_data_contact_name",
+                  "field_data_field_data_description",
+                  "field_data_field_data_resource",
+                  "field_data_field_data_resources_data",
+                  "field_data_field_distribution",
+                  "field_data_field_formatlist",
+                  "field_data_field_geo",
+                  "field_data_field_language",
+                  "field_data_field_lisens",
+                  "field_data_field_los",
+                  "field_data_field_organization",
+                  "field_data_field_user_org_ref",
+                  "field_data_title_field"
+]
+
+table_structure = {
+    "field_data_field_access_rights": { "short": "ar", "valuefields": [{"valuefield" : "field_access_rights_target_id", "isnode" : True}]},
+    "field_data_field_approved": { "short": "a", "valuefields": [{"valuefield": "field_approved_value", "isnode" : False}]},
+    "field_data_field_category": { "short": "c", "valuefields": [{"valuefield": "field_category_tid", "isnode" : False}]},
+    "field_data_field_data_contact_mail": { "short": "cm", "valuefields": [{"valuefield": "field_data_contact_mail_value", "isnode" : False}]},
+    "field_data_field_data_contact_name": { "short": "cn", "valuefields": [{"valuefield": "field_data_contact_name_value", "isnode" : False}]},
+    "field_data_field_data_description": { "short": "desc", "valuefields": [{"valuefield": "field_data_description_value", "isnode" : False}]},
+    "field_data_field_data_resource": { "short": "r", "valuefields": [{"valuefield": "field_data_resource_url", "isnode": False}, {"valuefield": "field_data_resource_title", "isnode": False}]},
+    "field_data_field_data_resources_data": { "short": "rd", "valuefields": [{"valuefield": "field_data_resources_data_url", "isnode": False}, {"valuefield": "field_data_resources_data_title", "isnode": False}]},
+    "field_data_field_distribution": { "short": "dist", "valuefields": [{"valuefield": "field_distribution_target_id", "isnode" : True}]},
+    "field_data_field_formatlist": { "short": "f", "valuefields": [{"valuefield": "field_formatlist_target_id", "isnode" : True}]},
+    "field_data_field_geo": { "short": "geo", "valuefields": [{"valuefield": "field_geo_tid", "isnode" : False}]},
+    "field_data_field_language": { "short": "lang", "valuefields": [{"valuefield": "field_language_target_id", "isnode" : True}]},
+    "field_data_field_lisens": { "short": "lis", "valuefields": [{"valuefield": "field_lisens_licence", "isnode" : False}, {"valuefield":"field_lisens_name", "isnode" : False}, {"valuefield": "field_lisens_url", "isnode" : False}]},
+    "field_data_field_los": { "short": "los", "valuefields": [{"valuefield": "field_los_tid", "isnode" : False }]},
+    "field_data_field_organization": { "short": "o", "valuefields": [{"valuefield": "field_organization_nid", "isnode" : True}]},
+    "field_data_field_user_org_ref": { "short": "uor", "valuefields": [{"valuefield": "field_user_org_ref_nid", "isnode" : True}]},
+    "field_data_title_field": { "short": "tf", "valuefields": [{"valuefield": "title_field_value", "isnode": False}]}
+}
