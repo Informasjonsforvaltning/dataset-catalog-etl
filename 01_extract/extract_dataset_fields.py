21ae373d680fecd92e09fdca8330fc31796f369b
valosnah <exthos@brreg.no>
2020-04-30 14:22:05 +0200

    chore: working extract for all dataset fields

--- 01_extract/extract_dataset_fields.py
+++ 01_extract/extract_dataset_fields.py
@@ -1,28 +1,33 @@
 
+
 from sqlalchemy import create_engine
 from fields import fields
 import pymysql
 import pandas as pd
+from pathlib import Path
+import json
 
-datasets = {"0": "77","1": "78"}
+f = open('../tmp/datasets.json')
+datasets = json.load(f)["nid"]
 
 sqlEngine       = create_engine('mysql+pymysql://user:password@127.0.0.1', pool_recycle=3600)
 dbConnection    = sqlEngine.connect()
 
 
 for index in datasets:
-
+    entity_id = str(datasets[index])
+    Path("../tmp/dataset/" + entity_id).mkdir(parents=True, exist_ok=True)
     for field in fields:
         frame           = pd.read_sql("""
         SELECT *
         FROM
           drupal_datanorge.""" + field + """
         WHERE
-          entity_id = '""" + datasets[index] + """'
+          entity_id = '""" + entity_id + """'
         """, dbConnection)
 
         pd.set_option('display.expand_frame_repr', False)
-        frame.to_json(r'../tmp/dataset/' + datasets[index] + '-' + field + '.json')
+        frame.to_json(r'../tmp/dataset/' + entity_id + '/' + field + '.json')
 
 
 
