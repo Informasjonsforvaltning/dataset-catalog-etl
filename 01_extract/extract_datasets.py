22d46ccf01293aa3b5e4ace26346c60ada089a84
valosnah <exthos@brreg.no>
2020-04-30 14:32:38 +0200

    chore: minor rework, put scripts in one file

--- 01_extract/extract_datasets.py
+++ 01_extract/extract_datasets.py
@@ -1,11 +1,14 @@
 
 from sqlalchemy import create_engine
+from fields import fields
 import pymysql
 import pandas as pd
+from pathlib import Path
+import json
  
 sqlEngine       = create_engine('mysql+pymysql://user:password@127.0.0.1', pool_recycle=3600)
 dbConnection    = sqlEngine.connect()
-frame           = pd.read_sql("""
+dataset_frame           = pd.read_sql("""
 SELECT nid
 FROM
   drupal_datanorge.node
@@ -14,6 +17,25 @@ WHERE
 """, dbConnection)
  
 pd.set_option('display.expand_frame_repr', False)
-frame.to_json(r'../tmp/datasets.json')
- 
+dataset_frame.to_json(r'../tmp/datasets.json')
+
+f = open('../tmp/datasets.json')
+datasets = json.load(f)["nid"]
+
+
+for index in datasets:
+    entity_id = str(datasets[index])
+    Path("../tmp/dataset/" + entity_id).mkdir(parents=True, exist_ok=True)
+    for field in fields:
+        field_frame           = pd.read_sql("""
+        SELECT *
+        FROM
+          drupal_datanorge.""" + field + """
+        WHERE
+          entity_id = '""" + entity_id + """'
+        """, dbConnection)
+
+        pd.set_option('display.expand_frame_repr', False)
+        field_frame.to_json(r'../tmp/dataset/' + entity_id + '/' + field + '.json')
+
 dbConnection.close()
