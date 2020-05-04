c955b232bd5b7131164b8d1275b39e5544b961b3
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-04 19:51:22 +0200

    fix: set language as nb when not nb or en

--- 02_transform/extract_methods/description.py
+++ 02_transform/extract_methods/description.py
@@ -7,16 +7,20 @@ def entityDescription(entity_id):
     description_file = open('../tmp/extract/data/' + entity_id + '/field_data_field_data_description.json')
     description_field = json.load(description_file)
 
-    for index in description_field['field_data_description_value']:
-        lang = description_field['language'][index]
-        if(isEnOrNb(lang)):
-            description[lang] = stripHtml(description_field['field_data_description_value'][index])
+    
+    if len(description_field['field_data_description_value']) == 1 and not isEnOrNb(description_field['language']['0']):
+        description['nb'] = description_field['field_description_value']['0']
+    else:
+        for index in description_field['field_data_description_value']:
+            lang = description_field['language'][index]
+            if(isEnOrNb(lang)):
+                description[lang] = stripHtml(description_field['field_data_description_value'][index])
     
     return description
 
 def stripHtml(value):
     tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
-    return removeExtraSpaces(tag_re.sub(' ', value))
+    return removeExtraSpaces(tag_re.sub(' ', value.replace('&nbsp;', ' ')))
 
 def removeExtraSpaces(value):
     return re.sub(' +', ' ', value.strip())
--- 02_transform/extract_methods/title.py
+++ 02_transform/extract_methods/title.py
@@ -6,9 +6,13 @@ def entityTitle(entity_id):
     title_file = open('../tmp/extract/data/' + entity_id + '/field_data_title_field.json')
     title_field = json.load(title_file)
 
-    for index in title_field['title_field_value']:
-        lang = title_field['language'][index]
-        if(isEnOrNb(lang)):
-            title[lang] = title_field['title_field_value'][index]
+    
+    if len(title_field['title_field_value']) == 1 and not isEnOrNb(title_field['language']['0']):
+        description['nb'] = title_field['title_field_value']['0']
+    else:
+        for index in title_field['title_field_value']:
+            lang = title_field['language'][index]
+            if(isEnOrNb(lang)):
+                title[lang] = title_field['title_field_value'][index]
     
     return title
