68f5fd100c225b23c48755e8a1e7d826cd16398c
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-04 09:41:02 +0200

    chore: transform description

--- 02_transform/transform_datasets.py
+++ 02_transform/transform_datasets.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+from utils import isEnOrNb
+
 f = open('../tmp/extract/data.json')
 datasets = json.load(f)["nid"]
 
@@ -15,7 +17,19 @@ for dataset_index in datasets:
     title_field = json.load(title_file)
 
     for index in title_field['title_field_value']:
-        fdk_dataset['title'][title_field['language'][index]] = title_field['title_field_value'][index]
+        lang = title_field['language'][index]
+        if(isEnOrNb(lang)):
+            fdk_dataset['title'][lang] = title_field['title_field_value'][index]
+
+    # Extract description
+    fdk_dataset['description'] = {}
+    description_file = open('../tmp/extract/data/' + ds_id + '/field_data_field_data_description.json')
+    description_field = json.load(description_file)
+
+    for index in description_field['field_data_description_value']:
+        lang = description_field['language'][index]
+        if(isEnOrNb(lang)):
+            fdk_dataset['description'][lang] = description_field['field_data_description_value'][index]
 
     # Save dataset to file
     with open('../tmp/transform/dataset/' + ds_id + '.json', 'w') as outfile:
---
+++ 02_transform/utils.py
@@ -0,0 +1,5 @@
+def isEnOrNb(language):
+    if(language == 'en' or language == 'nb'):
+        return True
+    else:
+        return False
