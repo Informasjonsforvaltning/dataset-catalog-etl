cd1b751da8587e03ac822d6f01eb22c092f785ab
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-05 12:59:57 +0200

    feat: transform dataset distributions

--- 02_transform/extract_methods/__init__.py
+++ 02_transform/extract_methods/__init__.py
@@ -2,3 +2,4 @@ from .title import entityTitle
 from .description import entityDescription
 from .landing_page import entityLandingPage
 from .timestamp import dateFromTimestamp
+from .distribution import datasetDistributions
