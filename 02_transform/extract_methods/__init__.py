e43b2189ef6688598d12e95b4cdba8b21ceb0c5a
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-06 09:36:18 +0200

    feat: dataset language

--- 02_transform/extract_methods/__init__.py
+++ 02_transform/extract_methods/__init__.py
@@ -4,3 +4,4 @@ from .landing_page import entityLandingPage
 from .timestamp import dateFromTimestamp
 from .distribution import datasetDistributions
 from .access_rights import datasetAccessRights
+from .language import datasetLanguage
