03e24d49b8961c33e67b83de800a148d755d7a8b
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-01-12 15:52:12 +0100

    chore: new mongodb-to-mongodb etl

--- files/extract_datasets.py
+++ files/extract_datasets.py
@@ -3,22 +3,32 @@ import requests
 
 import argparse
 token_file = open('./tmp/token.txt')
-output_file = open('./tmp/datasets.json', 'w')
+# output_file = open('./tmp/datasets.json', 'w')
+catalogs = "./tmp/catalogs.json"
 
 token = str([line.rstrip('\n') for line in token_file][0])
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
 
-url = 'http://dataset-catalog:8080/datasets'
-headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + token}
+with open(catalogs) as catalog_file:
 
-print("Getting the following url: ", url)
+    embedded = json.load(catalog_file).get("_embedded")
+    data = embedded.get("catalogs") if embedded else []
 
-try:
-    rsp = requests.get(url, headers=headers)
-    rsp.raise_for_status()
-    output_file.write(rsp.text)
+    for catalog in data:
+        orgId = catalog['id']
 
-except requests.HTTPError as err:
-    print(f'{err}')
+        url = 'http://dataset-catalog:8080/catalogs' + orgId + '/datasets'
+        headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + token}
+        print("Getting the following url: ", url)
+
+        with open(f'./tmp/datasets-{orgId}.json', 'w') as output_file:
+
+            try:
+                rsp = requests.get(url, headers=headers)
+                rsp.raise_for_status()
+                output_file.write(rsp.text)
+
+            except requests.HTTPError as err:
+                print(f'{err}')
