1b7040c85e84e2e62ebff09338454bc51b648aa3
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-01-12 15:21:57 +0100

    chore: new mongodb-to-mongodb etl

--- files/extract_catalogs.py
+++ files/extract_catalogs.py
@@ -3,24 +3,22 @@ import requests
 
 import argparse
 token_file = open('./tmp/token.txt')
+output_file = open('./tmp/catalogs.json', 'w')
 
 token = str([line.rstrip('\n') for line in token_file][0])
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
 
-output_file = "./tmp/catalogs.json"
 url = 'http://dataset-catalog:8080/catalogs'
 headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + token}
 
 print("Getting the following url: ", url)
 
-with open(output_file) as output:
+try:
+    rsp = requests.get(url, headers=headers)
+    rsp.raise_for_status()
+    output_file.write(rsp.text)
 
-    try:
-        rsp = requests.get(url, headers=headers)
-        rsp.raise_for_status()
-        output.write(rsp.text)
-
-    except requests.HTTPError as err:
-        print(f'{err}')
+except requests.HTTPError as err:
+    print(f'{err}')
