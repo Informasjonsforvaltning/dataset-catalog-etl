690c123837cfcdd7f65bab974463da088dfcc769
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-11 16:29:16 +0100

    chore: install pymongo

--- files/extract_datasets.py
+++ files/extract_datasets.py
@@ -15,9 +15,9 @@ datasets = {}
 for id_dict in dict_list:
     print(str(id_dict))
     id_str = id_dict["_id"]
-    downloadURL = id_dict["downloadURL"] if id_dict["downloadURL"] else None
-    accessURL = id_dict["accessURL"] if id_dict["accessURL"] else None
-    conformsTo = id_dict["conformsTo"] if id_dict["conformsTo"] else None
+    downloadURL = id_dict["downloadURL"] if id_dict.get("downloadURL") else None
+    accessURL = id_dict["accessURL"] if id_dict.get("accessURL") else None
+    conformsTo = id_dict["conformsTo"] if id_dict.get("conformsTo") else None
     datasets["_id"] = id_str
     datasets["_id"].append(downloadURL)
     datasets["_id"].append(accessURL)
