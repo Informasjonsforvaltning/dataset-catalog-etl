b44705ebae5d543b244b613c91c8ff3217f91b6a
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-17 10:06:16 +0100

    chore: fixed load

--- files/load_datasets.py
+++ files/load_datasets.py
@@ -15,6 +15,6 @@ with open(args.outputdirectory + 'datasets_transformed.json') as transformed_fil
     transformed_json = json.load(transformed_file)
 
     for mongo_id in transformed_json:
-        to_be_updated = transformed_json[mongo_id]
+        to_be_updated = {"distribution": transformed_json[mongo_id]}
         print("Mongo_id: " + mongo_id + " || To be updated: " + str(to_be_updated))
         print(db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
