77a90a63c93a6ca71e72650ba84b0101fe15fa88
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-17 08:55:32 +0100

    chore: fixed extract + transform

--- files/extract_datasets.py
+++ files/extract_datasets.py
@@ -16,15 +16,8 @@ for id_dict in dict_list:
     dataset = {}
     id_str = id_dict["_id"]
     dist_list = id_dict.get("distribution")
-    dist_list = dist_list if dist_list else []
-    for dist in dist_list:
-        if id_dict.get("downloadURL"):
-            dataset["downloadURL"] = id_dict["downloadURL"]
-        if id_dict.get("accessURL"):
-            dataset["accessURL"] = id_dict["accessURL"]
-        if id_dict.get("conformsTo"):
-            dataset["conformsTo"] = id_dict["conformsTo"]
-    datasets[id_str] = dataset
+    if dist_list and len(dist_list) > 0:
+        datasets[id_str] = dist_list
 
 
 with open(args.outputdirectory + 'mongo_datasets.json', 'w', encoding="utf-8") as outfile:
