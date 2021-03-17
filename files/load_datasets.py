840a6347d48080124b48bd23597850d62c4815a0
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-17 09:41:41 +0100

    chore: fixed load

--- files/load_datasets.py
+++ files/load_datasets.py
@@ -15,14 +15,6 @@ with open(args.outputdirectory + 'datasets_transformed.json') as transformed_fil
     transformed_json = json.load(transformed_file)
 
     for mongo_id in transformed_json:
-        to_be_updated = {}
-        values = transformed_json[mongo_id]
-        if values.get("downloadURL"):
-            to_be_updated["downloadURL"] = values["downloadURL"]
-        if values.get("accessURL"):
-            to_be_updated["accessURL"] = values["accessURL"]
-        if values.get("conformsTo"):
-            to_be_updated["conformsTo"] = values["conformsTo"]
-        if to_be_updated:
-            print("Mongo_id: " + mongo_id + " || To be updated: " + str(to_be_updated))
-#            print(db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
+        to_be_updated = transformed_json[mongo_id]
+        print("Mongo_id: " + mongo_id + " || To be updated: " + str(to_be_updated))
+#        print(db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
