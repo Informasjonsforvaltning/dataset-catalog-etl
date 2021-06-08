8e3429938e6484847290ca173d08fed069a1470c
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-06-08 15:45:30 +0200

    chore: ETL for dataset uris

--- files/load_datasets.py
+++ files/load_datasets.py
@@ -3,20 +3,21 @@ import os
 from pymongo import MongoClient
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
 connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
-
 db = connection.datasetCatalog
-dict_list = list(db.catalogs.find())
-catalogs = {}
-for id_dict in dict_list:
-    id_str = id_dict["_id"]
-    catalogs[id_str] = {}
-    catalogs[id_str]["uri"] = id_dict.get("uri")
-print("Total number of extracted catalogs: " + str(len(catalogs)))
 
-with open(args.outputdirectory + 'mongo_catalogs.json', 'w', encoding="utf-8") as outfile:
-    json.dump(catalogs, outfile, ensure_ascii=False, indent=4)
+with open(args.outputdirectory + 'datasets_transformed.json') as datasets_file:
+    transformed_json = json.load(datasets_file)
+
+    total_updated = 0
+    for mongo_id in transformed_json:
+        to_be_updated = transformed_json[mongo_id]
+        print(db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
+        total_updated += 1
+    print("Total number of datasets updated: " + str(total_updated))
+
