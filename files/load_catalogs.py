6affe831fc33526671eae326e68a297e485262bb
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-06-14 12:55:01 +0200

    chore: ETL for dataset references uris prod

--- files/load_catalogs.py
+++ files/load_catalogs.py
@@ -15,8 +15,17 @@ with open(args.outputdirectory + 'transformed_datasets.json') as catalogs_file:
     transformed_json = json.load(catalogs_file)
 
     total_updated = 0
+    total_failed = 0
     for mongo_id in transformed_json:
         to_be_updated = transformed_json[mongo_id]
-        print(db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
+        print("Updating ID: " + mongo_id)
+        insert_result = db.datasets.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated})
+        if insert_result:
+            total_updated += 1
+            print("Successfully updated: " + mongo_id)
+        else:
+            total_failed += 1
+            print("Update failed: " + mongo_id)
         total_updated += 1
-    print("Total number of catalogs updated: " + str(total_updated))
+    print("Total number of datasets updated: " + str(total_updated))
+    print("Total number of datasets updates failed: " + str(total_failed))
