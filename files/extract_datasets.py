811f8865e3b51aca2e6953d3552ef7c8bea28455
Hans Olav Sund <hans.olav.sund@digdir.no>
2022-01-13 12:28:26 +0100

    chore: ETL ready for staging

--- files/extract_datasets.py
+++ files/extract_datasets.py
@@ -2,6 +2,7 @@ import json
 import os
 from pymongo import MongoClient
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
@@ -10,21 +11,41 @@ connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
 
 db = connection['datasetCatalog']
-
 dataset_list = list(db.datasets.find())
 datasets = {}
 skipped_datasets = 0
+
+
+def filter_legal_basis(legal_basis):
+    filtered = []
+    if legal_basis and len(legal_basis) > 0:
+        for basis in legal_basis:
+            uri = basis.get("uri")
+            if uri and len(uri) > 0:
+                filtered.append(basis)
+    return filtered
+
+
 for id_dict in dataset_list:
-    subject = id_dict.get("subject")
-    if subject and len(subject) > 0:
+    accessRights = id_dict.get("accessRights")
+    legalBasisForRestriction = filter_legal_basis(id_dict.get("legalBasisForRestriction"))
+    legalBasisForProcessing = filter_legal_basis(id_dict.get("legalBasisForProcessing"))
+    legalBasisForAccess = filter_legal_basis(id_dict.get("legalBasisForAccess"))
+
+    if len(legalBasisForAccess) == 0 and len(legalBasisForProcessing) == 0 and len(legalBasisForRestriction) == 0:
+        skipped_datasets += 1
+    elif accessRights and accessRights.get("uri") and re.search("RESTRICTED$", accessRights):
         _id = id_dict["_id"]
         datasets[_id] = {}
-        datasets[_id]["subject"] = subject
+        datasets[_id]["accessRights"] = accessRights
+        datasets[_id]["legalBasisForRestriction"] = legalBasisForRestriction
+        datasets[_id]["legalBasisForProcessing"] = legalBasisForProcessing
+        datasets[_id]["legalBasisForAccess"] = legalBasisForAccess
     else:
         skipped_datasets += 1
 print("Total number of datasets: " + str(len(dataset_list)))
 print("Total number of extracted datasets: " + str(len(datasets)))
-print("Total number of skipped datasets (no subject present): " + str(skipped_datasets))
+print("Total number of skipped datasets (no accessRights or legalBasis present): " + str(skipped_datasets))
 
 with open(args.outputdirectory + 'mongo_datasets.json', 'w', encoding="utf-8") as outfile:
     json.dump(datasets, outfile, ensure_ascii=False, indent=4)
--- files/transform_datasets.py
+++ files/transform_datasets.py
@@ -1,63 +1,20 @@
 import json
 import argparse
-import os
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-old_base_uri = os.environ['OLD_BASE_URI']
-new_base_uri = os.environ['NEW_BASE_URI']
-really_old_base_uri = os.environ['REALLY_OLD_BASE_URI']
 
 
 def transform(d_file):
     datasets = openfile(d_file)
     transformed_datasets = {}
-    transformed_count = 0
-    failed_count = 0
-    failed = {}
+    non_public = {"accessRights": {"uri": "http://publications.europa.eu/resource/authority/access-right/NON_PUBLIC"}}
     for dataset_key in datasets:
-        result = transform_dataset(datasets[dataset_key])
-        if result["transformed"]:
-            transformed_datasets[dataset_key] = result["transformed"]
-            transformed_count += 1
-        else:
-            failed[dataset_key] = datasets[dataset_key]
-            failed_count += 1
-    print("Total number of transformed datasets: " + str(transformed_count))
-    print("Total number of non-transformed datasets: " + str(failed_count))
-    with open(args.outputdirectory + "not_transformed.json", 'w', encoding="utf-8") as err_file:
-        json.dump(failed, err_file, ensure_ascii=False, indent=4)
+        transformed_datasets[dataset_key] = non_public
+    print("Total number of transformed datasets: " + str(len(transformed_datasets)))
     return transformed_datasets
-
-
-def transform_dataset(dataset):
-    subjects = dataset.get("subject")
-    modified_subjects = []
-    has_been_modified = False
-    for subject in subjects:
-        modified_url = replace_url(subject.get("identifier"))
-        if modified_url:
-            has_been_modified = True
-            modified_subject = subject
-            modified_subject["identifier"] = modified_url
-            modified_subjects.append(modified_subject)
-        else:
-            modified_subjects.append(subject)
-    if has_been_modified:
-        transformed_dataset = {"subject": modified_subjects}
-        return {"transformed": transformed_dataset}
-    else:
-        return {"transformed": None}
-
-
-def replace_url(url):
-    if old_base_uri in url:
-        return url.replace(old_base_uri, f'{new_base_uri}/collections')
-    elif really_old_base_uri in url:
-        return url.replace(really_old_base_uri, f'{new_base_uri}/collections')
-    return None
 
 
 def openfile(file_name):
